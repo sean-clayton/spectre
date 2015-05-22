65f5328d61c6c4c2f8f70f2d2bc34d16785ea838
Sean Clayton <clayton.sean@gmail.com>
2015-05-22 16:32:57 -0400

    Removed react stuff and renamed main js file

--- app/bootstrap.js
+++
@@ -1,8 +0,0 @@
-var config = require('./config.json');
-var themeName = config.theme.name;
-var themeFile = './css/' + process.platform + '.css';
-var theme = document.createElement('link');
-theme.href = themeFile;
-theme.rel = 'stylesheet';
-var head = document.getElementsByTagName('head')[0];
-head.appendChild(theme);
--- app/scripts/app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Titlebar from './components/titlebar.jsx'
-import Navbar from './components/navbar.jsx'
-
-window.React = React;
-React.render(<Navbar />, document.body);
-React.render(<Titlebar />, document.body);
